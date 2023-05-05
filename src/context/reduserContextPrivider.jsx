73311aa566d7299f86f3df3ea34930f36c79ce1a
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-05-06 00:03:41 +0430

    Add the codes related to add items to the reducer function

--- src/context/reduserContextPrivider.jsx
+++ src/context/reduserContextPrivider.jsx
@@ -5,6 +5,24 @@ const initialState={
     total:0,
     checkout:false,
 }
+const cardReduser=(state,action)=>{
+  switch (action.type){
+    case "ADD_ITEMS":
+        if(!selectedItems.filter(item=>item.id===action.payload.id)){
+            selectedItems.push({
+                ...action.payload,
+                quantity:1,
+            })
+        }
+        return({
+            ...state,
+            selectedItems:[...state.selectedItems]
+        })
+    
+
+  }  
+
+}
 const ReduserContextPrivider = () => {
     const [state,dispatch]=useReducer(initialState,cardReduser);
     return (
