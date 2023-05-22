85f62410d2f74feb928988a09a1c9fe263128a81
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-05-23 01:44:03 +0430

    styles added to detail component

--- src/components/shared/ProductDetails.jsx
+++ src/components/shared/ProductDetails.jsx
@@ -1,29 +1,36 @@
 import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { productsContext } from "../../context/productsPrivider";
-
+import styles from "../../styles/css/detail.module.css";
 const ProductDetails = () => {
- 
   const data = useContext(productsContext);
-   
-  const params=useParams(); 
-  const id=params.id;
-   
-  const product=data[id - 1];
-  
+
+  const params = useParams();
+  const id = params.id;
+
+  const product = data[id - 1];
+
   const { image, title, description, category, price } = product;
-  return <div>
-    <img src={image} alt="product"/>
-    <div>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <p>category: {category}</p>
-        <div>
-            <span>price: {price}$</span>
+  return (
+    <div className={styles.container}>
+      <div className={styles.productContainer}>
+        <img className={styles.productImage} src={image} />
+        <div className={styles.detailContainer}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <span className={styles.category}>
+            <span>category: </span>
+            {category}
+          </span>
+
+          <div className={styles.boxes}>
+            <span>{price} $</span>
             <Link to="/products">Back to shop</Link>
+          </div>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default ProductDetails;
