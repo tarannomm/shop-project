39f17d864912fd0b2ececba770613416a13d14ad
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-05-29 12:23:47 +0430

    Steel related to empty shopping cart was added

--- src/components/shopCard.jsx
+++ src/components/shopCard.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { useContext } from 'react';
-import {reducerContext} from '../context/reducerContextProvider';
+import React from "react";
+import { useContext } from "react";
+import { reducerContext } from "../context/reducerContextProvider";
 import ShCard from "./shared/ShCard";
+import styles from "../styles/css/shopcart.module.css";
+import { Link } from "react-router-dom";
 import empty from "../images/empty.png";
 const ShopCard = () => {
-    const { state, dispatch } = useContext(reducerContext);
-    return (
-        <div>
-             {state.selectedItems.length?state.selectedItems.map(item=><ShCard key={item.id} product={item}/>):<div className={styles.empty}>the shop cart is empty!are you want to back to shop?</div>}
-            
+  const { state, dispatch } = useContext(reducerContext);
+  return (
+    <div>
+      {state.selectedItems.length ? (
+        state.selectedItems.map((item) => (
+          <ShCard key={item.id} product={item} />
+        ))
+      ) : (
+        <div className={styles.empty}>
+            <p>Your shopping cart is empty!!</p>
+          <img src={empty} alt="empty" />
+          <Link to="/products">Back to shop</Link>
+          
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ShopCard;
